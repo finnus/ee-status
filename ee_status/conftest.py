29cf5d610a85b06cdef7ed375b01c2776bee4bb3
finnus <finn@hees-web.de>
2022-06-16 18:09:27 +0200

    setup: initial bare cookiecutter-django

---
+++ ee_status/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from ee_status.users.models import User
+from ee_status.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
