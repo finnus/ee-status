92ff1b47bf88d413a142b0d54a82c5ddb09b296e
finnus <40977905+finnus@users.noreply.github.com>
2023-11-24 10:01:27 +0100

    first version of a (slow) mapping

--- ee_status/mastr_data/urls.py
+++ ee_status/mastr_data/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import rankings_view, search_view, totals_view
+from .views import multi_polygon_map, rankings_view, search_view, totals_view
 
 app_name = "mastr_data"
 urlpatterns = [
     path("", search_view, name="search"),
     path("totals", totals_view, name="totals"),
     path("rankings", rankings_view, name="rankings"),
+    path("multi_polygon_map/", multi_polygon_map, name="multi_polygon_map"),
 ]
