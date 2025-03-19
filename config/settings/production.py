96d61d25affb5a7828fa9b32a4b29cb9448163fe
finnus <40977905+finnus@users.noreply.github.com>
2025-03-19 21:44:58 +0100

    fix whitnoise staticfiles encoding error

--- config/settings/production.py
+++ config/settings/production.py
@@ -63,7 +63,7 @@ SECURE_CONTENT_TYPE_NOSNIFF = env.bool(
 
 # STATIC
 # ------------------------
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 # MEDIA
 # ------------------------------------------------------------------------------
 
